4aacc5babe16aef965dd92b67f95b4d45d268b76
abtinokhovat <abtinokhovat@gmail.com>
2023-08-16 20:48:13 +0330

    add basic command structure for application

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+)
 
+func main() {
+	if len(os.Args) <= 1 {
+		fmt.Println("help entity")
+		return
+	}
+	entity := os.Args[1]
+
+	if len(os.Args) <= 2 {
+		fmt.Println("help operation")
+		return
+	}
+	operation := os.Args[2]
+
+	fmt.Println("entity:", entity, "operation:", operation)
 }
