5c6a9b3ed4b37b680d75db14d3f828704dd7bcb6
abtinokhovat <abtinokhovat@gmail.com>
2023-08-18 20:04:28 +0330

    feat: add user repository as a singelton with constructor

--- repository/user.go
+++ repository/user.go
@@ -1,6 +1,17 @@
 package repository
 
-import "todo-cli-go/entity"
+import (
+	h "github.com/abtinokhovat/file-handler-go"
+	"sync"
+	"todo-cli-go/entity"
+)
+
+var (
+	once     sync.Once
+	instance *UserRepository
+)
+
+const path = ""
 
 type UserStorageAdapter interface {
 	Create(email, password string) error
@@ -8,6 +19,20 @@ type UserStorageAdapter interface {
 }
 
 type UserRepository struct {
+	handler h.FileIOHandler[entity.User]
+}
+
+func NewUserRepository(handler h.FileIOHandler[entity.User]) *UserRepository {
+	return &UserRepository{handler: handler}
+}
+
+func GetUserRepository() *UserRepository {
+	once.Do(func() {
+		serializer := h.NewJsonSerializer[entity.User]()
+		handler := h.NewJsonIOHandler[entity.User](path, serializer)
+		instance = NewUserRepository(handler)
+	})
+	return instance
 }
 
 func (u *UserRepository) Create(email, password string) error {
