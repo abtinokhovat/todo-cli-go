c2019fa50679e3a1dd6f781e4994c8fd4826e633
abtinokhovat <abtinokhovat@gmail.com>
2023-08-18 19:38:48 +0330

    refactor base logic and add auth logic to the application

--- main.go
+++ main.go
@@ -1,22 +1,89 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"todo-cli-go/cmd"
+	"todo-cli-go/entity"
+	"todo-cli-go/service"
 )
 
 func main() {
-	if len(os.Args) <= 1 {
-		fmt.Println("help entity")
+	scanner := bufio.NewScanner(os.Stdin)
+	authService := service.BuildUserService()
+
+	entityType, operation := parseArgs(os.Args)
+
+	user := handleAuthentication(scanner, authService, entityType, operation)
+	if user == nil {
 		return
 	}
-	entity := os.Args[1]
 
-	if len(os.Args) <= 2 {
-		fmt.Println("help operation")
-		return
+	initCommand := cmd.CommandBuilder(entityType, operation)
+
+	command := cmd.NewCommand(user)
+	runCommandLoop(command, initCommand, scanner)
+}
+
+func parseArgs(args []string) (string, string) {
+	entityType := "task"
+	operation := "list"
+
+	if len(args) > 1 {
+		entityType = args[1]
 	}
-	operation := os.Args[2]
+	if len(args) > 2 {
+		operation = args[2]
+	}
 
-	fmt.Println("entity:", entity, "operation:", operation)
+	return entityType, operation
+}
+
+func handleAuthentication(scanner *bufio.Scanner, authService *service.UserService, entityType, operation string) *entity.User {
+	if operation == "register" && entityType == "user" {
+		return registerAndLogin(scanner, authService)
+	}
+
+	return login(scanner, authService)
+}
+
+func login(scanner *bufio.Scanner, authService *service.UserService) *entity.User {
+	email := cmd.Scan(scanner, "Enter your email address")
+	password := cmd.Scan(scanner, "Enter your password")
+
+	user, err := authService.Login(email, password)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return user
+}
+
+func registerAndLogin(scanner *bufio.Scanner, authService *service.UserService) *entity.User {
+	email := cmd.Scan(scanner, "Enter your email address")
+	password := cmd.Scan(scanner, "Enter your password")
+
+	err := authService.Register(email, password)
+	if err != nil {
+		fmt.Println("Error on registering user:", err)
+	}
+
+	user, err := authService.Login(email, password)
+	if err != nil {
+		fmt.Println("Error on logging in user:", err)
+	}
+
+	return user
+}
+
+func runCommandLoop(command *cmd.Command, initCommand string, scanner *bufio.Scanner) {
+	if initCommand != "register-user" {
+		command.Execute(initCommand)
+	}
+
+	for {
+		commandString := cmd.Scan(scanner, "Enter the command you want to execute")
+		command.Execute(commandString)
+	}
 }
