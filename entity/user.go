bd2ca7d79cea2d556a50458ac032cb0b3eb9be57
abtinokhovat <abtinokhovat@gmail.com>
2023-08-19 05:16:38 +0330

    feat: add user constructor

--- entity/user.go
+++ entity/user.go
@@ -5,3 +5,17 @@ type User struct {
 	Email    string
 	Password string
 }
+
+func NewUser(id int, email, password string) *User {
+	hashed := hash(password)
+	return &User{
+		Id:       uint(id),
+		Email:    email,
+		Password: hashed,
+	}
+}
+
+func hash(str string) string {
+	// TODO: implement hashing
+	return str
+}
