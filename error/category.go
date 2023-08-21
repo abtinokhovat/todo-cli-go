22b73ba86f8e4c929f9e6a25a3b2bbbd843eae1f
abtinokhovat <abtinokhovat@gmail.com>
2023-08-21 14:31:02 +0330

    feat: implement category repository and add errors

--- error/category.go
+++ error/category.go
@@ -1,1 +1,8 @@
 package apperror
+
+import "errors"
+
+var (
+	ErrCategoryNotFound       = errors.New("category not found")
+	ErrCategoryNotFoundToEdit = errors.New("category not found to edit")
+)
--- repository/category.go
+++ repository/category.go
@@ -4,6 +4,7 @@ import (
 	fileHandler "github.com/abtinokhovat/file-handler-go"
 	"sync"
 	"todo-cli-go/entity"
+	apperror "todo-cli-go/error"
 )
 
 const categoryStoragePath = "storage/category.json"
@@ -14,10 +15,10 @@ var (
 )
 
 type CategoryStorageAdapter interface {
-	Create(userID int, title, color string) (*entity.Category, error)
-	Edit(id int, title, color string) (*entity.Category, error)
-	GetByID(id int) (*entity.Category, error)
-	GetAll() []*entity.Category
+	Create(title, color string, userID uint) (*entity.Category, error)
+	Edit(id uint, title, color string) (*entity.Category, error)
+	GetByID(id uint) (*entity.Category, error)
+	GetAll() ([]*entity.Category, error)
 }
 
 type CategoryRepository struct {
@@ -37,26 +38,70 @@ func NewCategoryRepository(handler fileHandler.FileIOHandler[entity.Category]) *
 	return &CategoryRepository{handler: handler}
 }
 
-func (r *CategoryRepository) Create(userId uint, title, color string) (*entity.Category, error) {
+func (r *CategoryRepository) Create(title, color string, userId uint) (*entity.Category, error) {
+	// generate new id
 	id := r.newID()
+
+	// make a new category entity
 	category := entity.NewCategory(id, title, color, userId)
-	r.handler.WriteOne()
-	//TODO implement me
-	panic("implement me")
+
+	// write category to the storage
+	err := r.handler.WriteOne(*category)
+	if err != nil {
+		return nil, err
+	}
+	return category, nil
 }
-func (r *CategoryRepository) Edit(id int, title, color string) (*entity.Category, error) {
-	//TODO implement me
-	panic("implement me")
+func (r *CategoryRepository) GetAll() ([]entity.Category, error) {
+	categories, err := r.handler.Read()
+	if err != nil {
+		return nil, err
+	}
+
+	return categories, nil
 }
-func (r *CategoryRepository) GetByID(id int) (*entity.Category, error) {
-	//TODO implement me
-	panic("implement me")
+func (r *CategoryRepository) GetByID(id uint) (*entity.Category, error) {
+	categories, err := r.GetAll()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, category := range categories {
+		if category.ID == id {
+			return &category, nil
+		}
+	}
+
+	return nil, apperror.ErrCategoryNotFound
 }
-func (r *CategoryRepository) GetAll() []*entity.Category {
-	//TODO implement me
-	panic("implement me")
+func (r *CategoryRepository) Edit(id uint, title, color string) (*entity.Category, error) {
+	categories, err := r.GetAll()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, category := range categories {
+		// update category data
+		if category.ID == id {
+			category.Title = title
+			category.Color = color
+		}
+
+		// delete all data in file and rewrite it
+		err := r.handler.DeleteAndWrite(categories)
+		if err != nil {
+			return nil, err
+		}
+
+		return &category, nil
+	}
+
+	return nil, apperror.ErrCategoryNotFoundToEdit
 }
 func (r *CategoryRepository) newID() uint {
-	//TODO implement me
-	panic("implement me")
+	categories, err := r.handler.Read()
+	if err != nil {
+		panic("could not get a new id for telegram error happened in reading file")
+	}
+	return uint(len(categories) + 1)
 }
