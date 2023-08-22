fcfe52cd5ffe575ed98679582cd74f7f2b94dbac
abtinokhovat <abtinokhovat@gmail.com>
2023-08-22 14:40:37 +0330

    test: add bad mocker that causes errors

--- repository/mocks_test.go
+++ repository/mocks_test.go
@@ -1,7 +1,22 @@
 package repository_test
 
+import "errors"
+
+const (
+	ErrOnReading          = "error on reading file"
+	ErrWriting            = "error on writing file"
+	ErrDeleting           = "error on deleting file"
+	ErrOnWritingOrReading = "error on deleting and writing file file"
+)
+
 type MockIOHandler[T any] struct {
 	storage *[]T
+}
+
+func NewMockIOHandler[T any](storage *[]T) *MockIOHandler[T] {
+	return &MockIOHandler[T]{
+		storage: storage,
+	}
 }
 
 func (h *MockIOHandler[T]) Read() ([]T, error) {
@@ -18,4 +33,23 @@ func (h *MockIOHandler[T]) DeleteAndWrite(data []T) error {
 func (h *MockIOHandler[T]) DeleteAll() error {
 	h.storage = new([]T)
 	return nil
+}
+
+type MockBadIOHandler[T any] struct {
+}
+
+func (m MockBadIOHandler[T]) Read() ([]T, error) {
+	return nil, errors.New(ErrOnReading)
+}
+
+func (m MockBadIOHandler[T]) WriteOne(data T) error {
+	return errors.New(ErrWriting)
+}
+
+func (m MockBadIOHandler[T]) DeleteAll() error {
+	return errors.New(ErrDeleting)
+}
+
+func (m MockBadIOHandler[T]) DeleteAndWrite(data []T) error {
+	return errors.New(ErrOnWritingOrReading)
 }
