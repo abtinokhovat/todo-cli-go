84fb282af04102dbb45dd86e96dabe9111dd70d1
abtinokhovat <abtinokhovat@gmail.com>
2023-08-25 19:09:47 +0330

    feat(category): add String() method

--- entity/category.go
+++ entity/category.go
@@ -1,17 +1,29 @@
 package entity
 
+import (
+	"todo-cli-go/color"
+)
+
 type Category struct {
-	ID     uint
-	Title  string
-	Color  string
-	UserID uint
+	ID        uint
+	Title     string
+	Color     string
+	UserID    uint
+	colorCode color.Color
 }
 
-func NewCategory(id uint, title, color string, userId uint) *Category {
+func NewCategory(id uint, title, colour string, userId uint) *Category {
+	colorCode := color.GetColor(colour)
+
 	return &Category{
-		ID:     id,
-		Title:  title,
-		Color:  color,
-		UserID: userId,
+		ID:        id,
+		Title:     title,
+		Color:     colour,
+		UserID:    userId,
+		colorCode: colorCode,
 	}
+}
+
+func (c *Category) String() string {
+	return color.Colorf(c.colorCode, "#%d-%s: 🚻%d", c.ID, c.Title, c.UserID)
 }
