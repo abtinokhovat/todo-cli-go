c9f3459340aee28dcdc7649b169d742ae76dac3f
abtinokhovat <abtinokhovat@gmail.com>
2023-08-25 18:38:48 +0330

    chore(error): add unauth error message

--- error/user.go
+++ error/user.go
@@ -5,4 +5,5 @@ import "errors"
 var (
 	ErrUserWrongPasswordOrEmail = errors.New("either your entered email or password is wrong")
 	ErrUserNotFound             = errors.New("user not found")
+	ErrUnauthorized             = errors.New("the request you are making is not authorized")
 )
