c5c2a82eeea33556d8c34cb000cd3a0676200fc3
abtinokhovat <abtinokhovat@gmail.com>
2023-08-25 19:06:49 +0330

    refactor(error): update error messages make them more clear by adding the "error" word to the first of hem

--- error/category.go
+++ error/category.go
@@ -3,6 +3,6 @@ package apperror
 import "errors"
 
 var (
-	ErrCategoryNotFound       = errors.New("category not found")
-	ErrCategoryNotFoundToEdit = errors.New("category not found to edit")
+	ErrCategoryNotFound       = errors.New("error: category not found")
+	ErrCategoryNotFoundToEdit = errors.New("error: category not found to edit")
 )
--- error/user.go
+++ error/user.go
@@ -3,7 +3,7 @@ package apperror
 import "errors"
 
 var (
-	ErrUserWrongPasswordOrEmail = errors.New("either your entered email or password is wrong")
-	ErrUserNotFound             = errors.New("user not found")
-	ErrUnauthorized             = errors.New("the request you are making is not authorized")
+	ErrUserWrongPasswordOrEmail = errors.New("error: either your entered email or password is wrong")
+	ErrUserNotFound             = errors.New("error: user not found")
+	ErrUnauthorized             = errors.New("error: the request you are making is not authorized")
 )
