8ed703ae34f87cd7c90bd0df7b31b30be420e937
abtinokhovat <abtinokhovat@gmail.com>
2023-08-27 03:12:53 +0330

    feat(date): add new io error

--- error/io.go
+++ error/io.go
@@ -3,8 +3,9 @@ package apperror
 import "errors"
 
 var (
-	ErrNotCorrectDigit    = errors.New("error: the entered value is not a valid digit")
-	ErrOnReading          = errors.New("error on reading file")
-	ErrOnWriting          = errors.New("error on writing file")
-	ErrOnWritingOrReading = errors.New("error on deleting and writing file file")
+	ErrNotCorrectDigit     = errors.New("error: the entered value is not a valid digit")
+	ErrNotInACorrectFormat = errors.New("error: the entered data is in current format")
+	ErrOnReading           = errors.New("error on reading file")
+	ErrOnWriting           = errors.New("error on writing file")
+	ErrOnWritingOrReading  = errors.New("error on deleting and writing file file")
 )
