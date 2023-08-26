59ae14b29565a79c72c0e1a035048eed73e3df03
abtinokhovat <abtinokhovat@gmail.com>
2023-08-26 19:44:14 +0330

    feat(task): replace time with date in task dueDate

--- entity/task.go
+++ entity/task.go
@@ -1,12 +1,43 @@
 package entity
 
-import "time"
+import (
+	"bytes"
+	"fmt"
+	"todo-cli-go/pkg/date"
+)
 
 type Task struct {
-	Id         uint
+	ID         uint
 	Title      string
-	DueDate    time.Time
+	DueDate    *date.Date
 	Done       bool
-	CategoryId uint
+	CategoryID uint
 	UserId     uint
+}
+
+func NewTask(id uint, title string, done bool, date *date.Date, categoryId, userID uint) *Task {
+	return &Task{
+		ID:         id,
+		Title:      title,
+		DueDate:    date,
+		Done:       done,
+		CategoryID: categoryId,
+		UserId:     userID,
+	}
+}
+
+func (t Task) String() string {
+	var buffer bytes.Buffer
+
+	if t.Done {
+		buffer.WriteString("[X] - ")
+	} else {
+		buffer.WriteString("[] - ")
+	}
+
+	buffer.WriteString(fmt.Sprintf("#%d-%s", t.ID, t.Title))
+	if t.DueDate != nil {
+		buffer.WriteString(fmt.Sprintf(" -> %v", t.DueDate))
+	}
+	return buffer.String()
 }
